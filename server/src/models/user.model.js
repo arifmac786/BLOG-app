bea67de910e5fa8f8ca32c75f173c16c0773909b
arifmac786 <arifmac78692@gmail.com>
2025-11-16 10:29:50 +0530

    userModel OK

---
+++ server/src/models/user.model.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (plainPassword) {
+  return await bcrypt.compare(plainPassword, this.password);
+};
+
+userSchema.methods.generateAuthToken = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.JWT_TOKEN_SECRET,
+    {
+      expiresIn: process.env.JWT_TOKEN_EXPIRES,
+    }
+  );
+};
+const User = mongoose.model("User", userSchema);
+export default User;
