ee05a8f81f411e888a1f3b2cc50571250b51790b
arifmac786 <arifmac78692@gmail.com>
2025-11-16 10:17:54 +0530

    ApiError,ApiResponse,asyncHandler and cloudinary UTILS OK

---
+++ server/src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    const res = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log(res);
+    return res;
+  } catch (error) {
+    console.log("Cloudinary Upload Error:", error);
+
+    // Remove file only if exists
+    if (fs.existsSync(localFilePath)) {
+      fs.unlinkSync(localFilePath);
+    }
+  }
+};
+
+export default uploadOnCloudinary;
